19a61619f82e1d33c1711107d7655140f1b86e56
Nawan Menandro <32107825+NawanMen@users.noreply.github.com>
2018-05-14 15:32:41 -0300

    Add files via upload

--- DoaLar/cadastro.php
+++ DoaLar/cadastro.php
@@ -1,138 +1,35 @@
-<!DOCTYPE html>
-<html lang="en">
-
-  <head>
-
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="shortcut icon" href="img/favicon.ico">
-
-    <title>DoaLar</title>
-
-    <!-- Bootstrap core CSS -->
-    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Custom fonts for this template -->
-    <link href="https://fonts.googleapis.com/css?family=Catamaran:100,200,300,400,500,600,700,800,900" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Lato:100,100i,300,300i,400,400i,700,700i,900,900i" rel="stylesheet">
-
-    <!-- Custom styles for this template -->
-    <link href="css/one-page-wonder.min.css" rel="stylesheet">
-
-  </head>
-
-  <body>
-
-    <!-- Navigation -->
-    <nav class="navbar navbar-expand-lg navbar-dark navbar-custom fixed-top">
-      <div class="container">
-        <a class="navbar-brand" href="home.html">Home</a>
-        <a class="navbar-brand" href="#">Doações</a>
-        <a class="navbar-brand" href="galeria.html">Galeria</a>
-        <a class="navbar-brand" href="faleConosco.php">Fale Conosco</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarResponsive">
-          <ul class="navbar-nav ml-auto">
-            <li class="nav-item">
-              <a class="nav-link" href="login.php">Login</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="cadastro.php">Cadastro</a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
-
-    <header class="masthead text-center text-white">
-      <div class="masthead-content">
-        <div class="container">
-          <h1 class="masthead-heading mb-0">Cadastre-se</h1>
-          <h2 class="masthead-subheading mb-0">E Junte-se a Nós</h2>
-          <a href="saibaMais.html" class="btn btn-primary btn-xl rounded-pill mt-5">Saiba Mais!</a>
-        </div>
-      </div>
-      <div class="bg-circle-1 bg-circle"></div>
-      <div class="bg-circle-2 bg-circle"></div>
-      <div class="bg-circle-3 bg-circle"></div>
-      <div class="bg-circle-4 bg-circle"></div>
-    </header>
-
-    <section>
-      <div class="container">
-        <div class="row align-items-left">
-          <div class="col-lg-6 order-1">
-            <div class="p-5">
-            </div>
-          </div>
-            <div class="p-5">
-              <h2 class="display-4">Cadastro</h2>
-<br>
-              <div class="form-group row">
-                <label for="nome"><h5>Nome Completo</h5></label></label>
-                <input type="text" class="form-control" id="nome" size="50">
-              </div>
-<br>
-              <div class="form-group row">
-                  <label for="nome"><h5>E-mail</h5></label>
-                  <input type="email" class="form-control" id="email" size="50">
-                </div>
-<br>
-              <div class="form-group row">
-                  <label for="nome"><h5>Telefone Fixo</h5></label>
-                  <input type="text" class="form-control" id="foneF" size="50">
-              </div>
-<br>
-              <div class="form-group row">
-                  <label for="nome"><h5>Telefone Móvel</h5></label>
-                  <input type="text" class="form-control" id="foneM" size="50">
-              </div>
-
-              <div class="form-group row">
-                  <label for="nome"><h5>Senha</h5></label>
-                  <input type="text" class="form-control" id="foneF" size="50">
-              </div>
-<br>
-    <div class="form-group row">
-    <h5>Deseja fazer parte do ranking dos doadores?</h5>
-    </div>
-    <div class="form-group row">
-      <div class="form-check">
-        <label class="form-check-label">
-          <input type="radio" class="form-check-input" name="optionsRadios" id="optionsRadios1" value="option1" checked="">
-          Sim
-        </label>
-<br>
-        <label class="form-check-label">
-          <input type="radio" class="form-check-input" name="optionsRadios" id="optionsRadios2" value="option2">
-          Não
-        </label>
-      </div>
-      </div>
-<br>
-              <button type="button" class="btn btn-primary">Concluir</button>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
-
-    <!-- Footer -->
-    <footer class="py-5 bg-black">
-      <div class="container">
-        <p class="m-0 text-center text-white small">Copyright by TCC DoaLar 2018</p>
-      </div>
-      <!-- /.container -->
-    </footer>
-
-    <!-- Bootstrap core JavaScript -->
-    <script src="vendor/jquery/jquery.min.js"></script>
-    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-
-  </body>
-
-</html>
+<?php 
+ 
+$nome = $_POST['nome'];
+$email = $_POST['email'];
+$foneFixo = $_POST['foneFixo'];
+$foneMovel = $_POST['foneMovel'];
+$senha = MD5($_POST['senha']);
+$connect = mysql_connect('nome_do_servidor','nome_de_usuario','senha');
+$db = mysql_select_db('nome_do_banco_de_dados');
+$query_select = "SELECT login FROM usuarios WHERE login = '$login'";
+$select = mysql_query($query_select,$connect);
+$array = mysql_fetch_array($select);
+$logarray = $array['login'];
+ 
+  if($nome == "" || $nome == null){
+    echo"<script language='javascript' type='text/javascript'>alert('O campo login deve ser preenchido');window.location.href='cadastro.html';</script>";
+ 
+    }else{
+      if($logarray == $nome){
+ 
+        echo"<script language='javascript' type='text/javascript'>alert('Esse login já existe');window.location.href='cadastro.html';</script>";
+        die();
+ 
+      }else{
+        $query = "INSERT INTO usuarios (login,senha) VALUES ('$nome','$senha')";
+        $insert = mysql_query($query,$connect);
+         
+        if($insert){
+          echo"<script language='javascript' type='text/javascript'>alert('Usuário cadastrado com sucesso!');window.location.href='login.html'</script>";
+        }else{
+          echo"<script language='javascript' type='text/javascript'>alert('Não foi possível cadastrar esse usuário');window.location.href='cadastro.html'</script>";
+        }
+      }
+    }
+?>
